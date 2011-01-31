1d9675472eafa84ecf58449aa4f407a82fb1f1f8
Kesavan Rengarajan <k7@trk7.com>
2011-01-31 13:33:04 +1100

    using the directory holding this script
    otherwise, if this script is called inside another script then the cfg
    and cookie files are going to be expected to exist in the parent scripts
    directory.

--- get_page.sh
+++ get_page.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-source profile.cfg
-wget --save-cookies cookies.txt --keep-session-cookies \
+cur_dir=$(dirname $0)
+source "$cur_dir/profile.cfg"
+wget --save-cookies "$cur_dir/cookies.txt" --keep-session-cookies \
                         --post-data "username=$username&password=$password" \
                         "$auth_uri" -O /dev/null >/dev/null 2>&1 
 if [ $? -eq 0 ]
 then
 # Now grab the page to be profiled
-wget --load-cookies cookies.txt \
+wget --load-cookies "$cur_dir/cookies.txt" \
     --server-response -p "$profile_uri" -O /dev/null >/dev/null 2>&1 
 fi
